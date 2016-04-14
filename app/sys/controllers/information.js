b1fe58a1368042d995bdfcba541eb4bfb30cc872
wjm <wjm>
2016-04-14 22:45:30 +0800

    issue#7

--- app/sys/controllers/information.js
+++ app/sys/controllers/information.js
@@ -4,70 +4,20 @@ angular
 
 function informationCtrl ($scope){
   var vm = this;
-  vm.itemsByPage = 5;
+  vm.itemsByPage = 10;
 
   Bmob.initialize('7e492cbe197a97cecf3c0ad72120dd04', 'a7d52f0f857157f328fc36600b5b0034');
 
-    var Info = Bmob.Object.extend('information');
-    var query = new Bmob.Query(Info);
-    query.select('name','no','remark');
-    //query.limit(10);
-    vm.info2 = [];
-    query.find().then(function (results){
-      vm.info = results;
-      _.forEach(vm.info,function (t){
-        vm.info2.push(t.attributes);
-      });
-      $scope.$apply();
-    })
-
-
-    /*var viewModel = {};
-    viewModel.data = ko.observableArray();
-    viewModel.initData = function() {
-        var Chat = Bmob.Object.extend('information');
-        var chat = new Bmob.Query(Chat);
-        chat.select('name','no','remark');
-        chat.find().then(function(results){
-            viewModel.data(results);
-        });
-    }();
-    ko.applyBindings(viewModel);
-
-    var test = viewModel;
-
-  console.log(test)*/
-
-
-
-/*  var
-      nameList = ['Pierre', 'Pol', 'Jacques', 'Robert', 'Elisa'],
-      familyName = ['Dupont', 'Germain', 'Delcourt', 'bjip', 'Menez'];
-
-  function createRandomItem() {
-      var
-          firstName = nameList[Math.floor(Math.random() * 4)],
-          lastName = familyName[Math.floor(Math.random() * 4)],
-          age = Math.floor(Math.random() * 100),
-          email = firstName + lastName + '@whatever.com',
-          balance = Math.random() * 3000;
-
-      return{
-          firstName: firstName,
-          lastName: lastName,
-          age: age,
-          email: email,
-          balance: balance
-      };
-  }
-
-  $scope.itemsByPage = 7;
-
-  $scope.rowCollection = [];
-  for (var j = 0; j < 30; j++) {
-      $scope.rowCollection.push(createRandomItem());
-  }
-
-  console.log('111',$scope.rowCollection)*/
+  var Info = Bmob.Object.extend('information');
+  var query = new Bmob.Query(Info);
+  query.select('name','no','remark');
+  vm.info2 = [];
+  query.find().then(function (results){
+    vm.info = results;
+    _.forEach(vm.info,function (t){
+      vm.info2.push(t.attributes);
+    });
+    $scope.$apply();
+  })
 
 }
